95aa945e3c7f67a2c7055a483c3c5a7cf4fe8d82
Michael Ronge <devel@ronge.org>
2018-09-05 20:50:30 +0200

    Attributkonverting weiter betrieben, aber noch nicht fertig.

--- CMDB/Admin/ConvertAttributeToItem.aspx
+++ CMDB/Admin/ConvertAttributeToItem.aspx
@@ -1,11 +1,64 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin/Admin.master" AutoEventWireup="true" CodeFile="ConvertAttributeToItem.aspx.cs" Inherits="Admin_ConvertAttributeToItem" %>
 
-<asp:Content ID="Content1" ContentPlaceHolderID="cphNav" Runat="Server">
+<asp:Content ID="Content1" ContentPlaceHolderID="cphNav" runat="Server">
 </asp:Content>
-<asp:Content ID="Content2" ContentPlaceHolderID="cphMainHeader" Runat="Server">
+<asp:Content ID="Content2" ContentPlaceHolderID="cphMainHeader" runat="Server">
+    <h1>Attribut-Typen in Item-Typen konvertieren</h1>
+    <h2>-
+        <asp:Label ID="lblTypeName" runat="server" />
+        -</h2>
 </asp:Content>
-<asp:Content ID="Content3" ContentPlaceHolderID="cphMainArticle" Runat="Server">
+<asp:Content ID="Content3" ContentPlaceHolderID="cphMainArticle" runat="Server">
+    <asp:ScriptManager runat="server" />
+    <asp:UpdatePanel runat="server">
+        <ContentTemplate>
+            <p></p>
+            <asp:MultiView ID="mvContent" runat="server" ActiveViewIndex="0" OnActiveViewChanged="mvContent_ActiveViewChanged">
+                <asp:View runat="server">
+                    <table>
+                        <tr>
+                            <td>Betroffene Item-Typen:</td>
+                            <td>
+                                <asp:ListBox ID="lstItemTypes" runat="server" 
+                                    DataTextField="TypeName" DataValueField="TypeId" Enabled="false" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Betroffene Configuration Items:</td>
+                            <td>
+                                <asp:Label ID="txtNumItems" runat="server" /></td>
+                        </tr>
+                        <tr>
+                            <td>Betroffene Attribute:</td>
+                            <td>
+                                <asp:Label ID="txtNumAttributes" runat="server" /></td>
+                        </tr>
+                        <tr>
+                            <td>Neu entstehende Configuration Items:</td>
+                            <td>
+                                <asp:Label ID="txtNumNewItems" runat="server" /></td>
+                        </tr>
+                    </table>
+                    <hr />
+                    <p>
+                        Der neue Item-Typ soll
+                        <asp:DropDownList ID="lstDirection" runat="server" AutoPostBack="true" OnSelectedIndexChanged="lstDirection_SelectedIndexChanged">
+                            <asp:ListItem Selected="True" Text="oberhalb" Value="1" />
+                            <asp:ListItem Text="unterhalb" Value="0" />
+                        </asp:DropDownList>
+                        des bisherigen Item-Typen mit dem Attribut stehen.
+                    </p>
+                    <p>Welcher Verbindungstyp soll verwendet werden?
+                        <asp:DropDownList ID="lstConnectionType" runat="server" DataTextField="ConnTypeName" DataValueField="ConnTypeId"
+                            OnPreRender="lstConnectionType_PreRender" /></p>
+                    <p>Welche Attribut-Typen sollen vom alten Item-Typ zum aus dem Attribut-Typ neu erstellten Item-Typ transferiert werden?</p>
+                    <asp:CheckBoxList ID="lstCorrespondingAttributeTypes" runat="server" DataTextField="TypeName" DataValueField="TypeId" />
+                </asp:View>
+            </asp:MultiView>
+            <asp:Label ID="lblError" runat="server" Visible="false" CssClass="errorlabel" />
+        </ContentTemplate>
+    </asp:UpdatePanel>
 </asp:Content>
-<asp:Content ID="Content4" ContentPlaceHolderID="cphAside" Runat="Server">
+<asp:Content ID="Content4" ContentPlaceHolderID="cphAside" runat="Server">
 </asp:Content>
 
--- CMDB/Admin/ConvertAttributeToItem.aspx.cs
+++ CMDB/Admin/ConvertAttributeToItem.aspx.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CmdbAPI.BusinessLogic;
+using CmdbAPI.TransferObjects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,8 +9,104 @@ using System.Web.UI.WebControls;
 
 public partial class Admin_ConvertAttributeToItem : System.Web.UI.Page
 {
+    private AttributeType attributeType;
+    private List<ItemType> itemTypes = new List<ItemType>();
+    private IEnumerable<ConfigurationItem> items;
+    private IEnumerable<ItemAttribute> attributes;
+    private IEnumerable<AttributeType> correspondingAttributeTypes;
+    private IEnumerable<ConnectionType> connectionTypes;
+    private Dictionary<string, List<Guid>> attributeValueItemsMapper = new Dictionary<string, List<Guid>>();
+    private Dictionary<string, ConnectionType> connTypes = new Dictionary<string, ConnectionType>();
+
     protected void Page_Load(object sender, EventArgs e)
     {
+        string guidString = Request.QueryString["ID"];
+        Guid guid;
+        if (string.IsNullOrWhiteSpace(guidString) || !Guid.TryParse(guidString, out guid))
+        {
+            Response.Redirect("~/Admin/AttributeTypes.aspx", true);
+            return;
+        }
+        attributeType = MetaDataHandler.GetAttributeType(guid);
+        if (attributeType == null)
+            Response.Redirect("~/Admin/AttributeTypes.aspx", true);
 
+        connectionTypes = MetaDataHandler.GetConnectionTypes();
+        foreach (ConnectionType connectionType in connectionTypes)
+        {
+            connTypes.Add(connectionType.ConnTypeId.ToString(), connectionType);
+        }
+
+        if (!IsPostBack)
+        {
+            List<string> itemTypeNames = new List<string>(MetaDataHandler.GetItemTypes().Select(i => i.TypeName));
+            if (itemTypeNames.Contains(attributeType.TypeName))
+            {
+                lblError.Text = "Ein gleichnamiger Item-Typ ist bereits vorhanden. Sie müssen den Namen des Attribut-Typs ändern.";
+                lblError.Visible = true;
+                mvContent.Visible = false;
+            }
+
+            RetrieveInformation();
+        }
+    }
+
+    private void RetrieveInformation()
+    {
+        lblTypeName.Text = attributeType.TypeName;
+        itemTypes.AddRange(MetaDataHandler.GetItemTypesByAllowedAttributeType(attributeType.TypeId));
+        items = DataHandler.GetConfigurationItemsByType(itemTypes.Select(t => t.TypeId).ToArray());
+        txtNumItems.Text = items.Count().ToString();
+        attributes = DataHandler.GetAttributeForAttributeType(attributeType.TypeId);
+        correspondingAttributeTypes = MetaDataHandler.GetAttributeTypesForCorrespondingValuesOfType(attributeType.TypeId);
+
+        string[] vals = attributes.Select(a => a.AttributeValue).Distinct().ToArray();
+        for (int i = 0; i < vals.Length; i++)
+        {
+            attributeValueItemsMapper.Add(vals[i], new List<Guid>());
+        }
+
+        txtNumAttributes.Text = attributes.Count().ToString();
+        txtNumNewItems.Text = vals.Length.ToString();
+        lstItemTypes.DataSource = itemTypes;
+        lstItemTypes.DataBind();
+        lstCorrespondingAttributeTypes.DataSource = correspondingAttributeTypes;
+        lstCorrespondingAttributeTypes.DataBind();
+        lstDirection_SelectedIndexChanged(null, null);
+    }
+
+    protected void mvContent_ActiveViewChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void lstDirection_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        string val = lstConnectionType.SelectedValue; // Ausgewählten Wert beibehalten, falls möglich
+        lstConnectionType.DataSource = connectionTypes;
+        lstConnectionType.DataBind();
+        if (!string.IsNullOrEmpty(val))
+            lstConnectionType.SelectedValue = val;
+    }
+
+    protected void lstConnectionType_PreRender(object sender, EventArgs e)
+    {
+        // Hilfsfunktion, die die DropDownList-Einträge neu schreibt, da Verbindungstypen in eine richtung gleich und die
+        // andere unterschiedlich heißen können.
+        foreach (ListItem item in lstConnectionType.Items)
+        {
+            ConnectionType ct = connTypes[item.Value];
+            switch (lstDirection.SelectedValue)
+            {
+                case "1": //oberhalb
+                    item.Text = string.Format("{0} ({1})", ct.ConnTypeName, ct.ConnTypeReverseName);
+                    break;
+                case "0": //unterhalb
+                    item.Text = string.Format("{0} ({1})", ct.ConnTypeReverseName, ct.ConnTypeName);
+                    break;
+                default:
+                    throw new Exception("Falscher Wert ausgewählt.");
+            }
+        }
     }
 }
