aea4b3d5377fbb8f521a36b8a92bcd5bf4cbaa51
Michael Ronge <devel@ronge.org>
2018-10-14 08:30:35 +0200

    History hinzugefügt, aber noch nicht beendet

---
+++ CMDB/ShowHistory.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/CMDB.master" AutoEventWireup="true" CodeFile="ShowHistory.aspx.cs" Inherits="ShowHistory" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="cphNav" runat="Server">
+    <ul>
+        <li>
+            <asp:HyperLink runat="server" NavigateUrl="~/HelpPages/showhistory.aspx" Target="help">?</asp:HyperLink>
+        </li>
+    </ul>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="cphMainHeader" runat="Server">
+    <h1>
+        <asp:Label ID="lblName" runat="server" />
+    </h1>
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="cphMainArticle" runat="Server">
+</asp:Content>
+<asp:Content ID="Content4" ContentPlaceHolderID="cphAside" runat="Server">
+    <ul>
+        <li>
+            <a href="ShowItem.aspx?id=<%: Request.QueryString["id"] %>" class="intern">Item anzeigen</a>
+        </li>
+    </ul>
+</asp:Content>
+
---
+++ CMDB/ShowHistory.aspx.cs
@@ -0,0 +1,31 @@
+﻿using CmdbAPI.BusinessLogic;
+using CmdbAPI.TransferObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class ShowHistory : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string idQueryString = Request.QueryString["id"];
+        Guid id = new Guid();
+        if (string.IsNullOrWhiteSpace(idQueryString) || !Guid.TryParse(idQueryString, out id))
+            Response.Redirect("~/Default.aspx", true);
+
+        (Master as CMDB).IsButtonCreateVisible = true;
+
+        ConfigurationItem item = DataHandler.GetConfigurationItem(id);
+        if (item == null)
+            Response.Redirect("~/Default.aspx", true);
+
+        lblName.Text = string.Format("{0}: {1}", item.TypeName, item.ItemName);
+
+        this.Title = string.Format("Veränderungen an {0}: {1} anzeigen", item.TypeName, item.ItemName);
+
+
+    }
+}
