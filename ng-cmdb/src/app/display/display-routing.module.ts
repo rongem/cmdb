ca6c2757d6fb9c61fd67ce0484980a398ae843de
Michael Ronge <devel@ronge.org>
2019-05-06 19:47:13 +0200

    Design verbessert, und Suche auf Hauptseite hinzugefügt

--- ng-cmdb/src/app/display/display-routing.module.ts
+++ ng-cmdb/src/app/display/display-routing.module.ts
@@ -3,13 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { DisplayComponent } from './display.component';
 import { ConfigurationItemComponent } from './configuration-item/configuration-item.component';
 import { ResultTableComponent } from './search/result-table/result-table.component';
+import { SearchFormComponent } from './search/search-form/search-form.component';
 
 const displayRoutes: Routes = [
     {
         path: 'display', component: DisplayComponent, children: [
             {
                 path: 'configuration-item', children: [
-                    { path: '', pathMatch: 'full', redirectTo: 'new' },
+                    { path: '', pathMatch: 'full', redirectTo: 'search' },
+                    { path: 'search', component: SearchFormComponent },
                     { path: 'results', component: ResultTableComponent },
                     { path: 'new', component: ConfigurationItemComponent, canActivate: [] },
                     { path: ':id/edit', component: ConfigurationItemComponent, canActivate: []},
