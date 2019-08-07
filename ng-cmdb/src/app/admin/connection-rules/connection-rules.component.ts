7a097c15d035a5e2d6e0855679f30e066c85ed14
Michael Ronge <github@ronge.org>
2019-08-07 07:50:43 +0200

    started with connection rules

--- ng-cmdb/src/app/admin/connection-rules/connection-rules.component.ts
+++ ng-cmdb/src/app/admin/connection-rules/connection-rules.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { Guid } from 'guid-typescript';
+
+import * as fromApp from 'src/app/shared/store/app.reducer';
+import * as fromMetaData from 'src/app/shared/store/meta-data.reducer';
+import * as MetaDataActions from 'src/app/shared/store/meta-data.actions';
+
 
 @Component({
   selector: 'app-connection-rules',
@@ -6,10 +15,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./connection-rules.component.scss']
 })
 export class ConnectionRulesComponent implements OnInit {
+  meta: Observable<fromMetaData.State>;
 
-  constructor() { }
+  constructor(private store: Store<fromApp.AppState>,
+              public dialog: MatDialog) { }
 
   ngOnInit() {
+    this.meta = this.store.select(fromApp.METADATA);
   }
 
 }
