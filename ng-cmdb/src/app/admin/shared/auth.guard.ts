e7377d54b4a1ae2a958a2de81b3ae1a9b163ebcb
Michael Ronge <devel@ronge.org>
2019-08-14 20:46:35 +0200

    corrected the problem, that user authentication needs longer than the authgard. Now the guard waits for authorization before it redirects

--- ng-cmdb/src/app/admin/shared/auth.guard.ts
+++ ng-cmdb/src/app/admin/shared/auth.guard.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { map, tap, take } from 'rxjs/operators';
+import { map, tap, take, skipWhile } from 'rxjs/operators';
 
 import * as fromApp from '../../shared/store/app.reducer';
 import { UserRole } from '../../shared/objects/user-role.enum';
@@ -12,7 +12,10 @@ export class AuthGuard implements CanActivate {
     constructor(private store: Store<fromApp.AppState>, private router: Router) {}
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-        return this.store.select(fromApp.METADATA).pipe(take(1), map(meta =>
+        return this.store.select(fromApp.METADATA).pipe(
+            skipWhile(meta => !meta.validData),
+            take(1),
+            map(meta =>
             meta.userRole === UserRole.Administrator ? true : this.router.createUrlTree(['/'])));
     }
 }
