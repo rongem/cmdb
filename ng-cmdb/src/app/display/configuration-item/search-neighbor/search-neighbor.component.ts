f350f33b47b7f825ff0f97e8bbdac8cadf19e959
Michael Ronge <devel@ronge.org>
2019-10-16 08:03:13 +0200

    work in progress neighbor search

--- ng-cmdb/src/app/display/configuration-item/search-neighbor/search-neighbor.component.ts
+++ ng-cmdb/src/app/display/configuration-item/search-neighbor/search-neighbor.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 import { Store, select } from '@ngrx/store';
 import { map } from 'rxjs/operators';
 
@@ -7,6 +8,7 @@ import * as fromApp from 'src/app/shared/store/app.reducer';
 import * as fromSelectDisplay from 'src/app/display/store/display.selectors';
 import * as SearchActions from 'src/app/display/store/search.actions';
 
+import { Guid } from 'src/app/shared/guid';
 
 @Component({
   selector: 'app-search-neighbor',
@@ -14,11 +16,28 @@ import * as SearchActions from 'src/app/display/store/search.actions';
   styleUrls: ['./search-neighbor.component.scss']
 })
 export class SearchNeighborComponent implements OnInit {
+  form: FormGroup;
 
   constructor(private store: Store<fromApp.AppState>,
-              private fb: FormBuilder) { }
+              private fb: FormBuilder,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.form = this.fb.group({
+        ItemType: Guid.EMPTY,
+        SourceItem: params.id,
+        MaxLevels: 5,
+        ExtraSearch: this.fb.group({
+          NameOrValue: '',
+          ItemType: undefined,
+          Attributes: this.fb.array([]),
+          ConnectionsToUpper: this.fb.array([]),
+          ConnectionsToLower: this.fb.array([]),
+          ResponsibleToken: '',
+        }),
+      });
+    });
   }
 
   get itemReady() {
