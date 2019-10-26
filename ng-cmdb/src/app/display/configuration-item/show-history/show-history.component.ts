3d63a7f642c2e1a313ed39846b4bcbc9c4f54193
Michael Ronge <github@ronge.org>
2019-10-26 08:10:12 +0200

    Filtering history works now

--- ng-cmdb/src/app/display/configuration-item/show-history/show-history.component.ts
+++ ng-cmdb/src/app/display/configuration-item/show-history/show-history.component.ts
@@ -1,16 +1,11 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { Store, select } from '@ngrx/store';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-
-import * as fromApp from 'src/app/shared/store/app.reducer';
 
 import { Guid } from 'src/app/shared/guid';
 import { HistoryEntry } from '../objects/history-entry.model';
 import { getUrl } from 'src/app/shared/store/functions';
-import { getRouterState } from 'src/app/shared/store/router/router.reducer';
+import { MatTableDataSource } from '@angular/material/table';
 
 
 @Component({
@@ -19,28 +14,22 @@ import { getRouterState } from 'src/app/shared/store/router/router.reducer';
   styleUrls: ['./show-history.component.scss']
 })
 export class ShowHistoryComponent implements OnInit {
-  private history: Observable<HistoryEntry[]>;
-  private id: Guid;
+  history: MatTableDataSource<HistoryEntry>;
   displayedColumns = ['date', 'subject', 'text', 'responsible'];
-  filter = '';
 
-  constructor(private store: Store<fromApp.AppState>,
-              public dialogRef: MatDialogRef<ShowHistoryComponent>,
+  constructor(public dialogRef: MatDialogRef<ShowHistoryComponent>,
               @Inject(MAT_DIALOG_DATA) public data: Guid,
               public dialog: MatDialog,
               private http: HttpClient) { }
 
   ngOnInit() {
-    this.store.pipe(select(getRouterState)).subscribe(state => this.id = state.state.params.id);
+    const sub = this.http.get<HistoryEntry[]>(
+      getUrl('ConfigurationItem/' + this.data + '/history')).subscribe(
+        entries => {
+          this.history = new MatTableDataSource(entries);
+          this.history.filterPredicate = (data, filter) => filter === '' || data.Scope === filter;
+          sub.unsubscribe();
+      }
+    );
   }
-
-  get historyEntries() {
-    if (!this.history) {
-      this.history = this.http.get<HistoryEntry[]>(
-        getUrl('ConfigurationItem/' + this.id + '/history')).pipe(
-          map(values => values.filter(value => this.filter === '' || value.Scope === this.filter)));
-    }
-    return this.history;
-  }
-
 }
