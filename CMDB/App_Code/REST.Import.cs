b71d824c9e1c08e39510cf374924627cc9244692
Michael Ronge <github@ronge.org>
2019-11-22 15:29:07 +0100

    work in progress; problems with file transfer between angular and wcf

---
+++ CMDB/App_Code/REST.Import.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Web;
+
+/// <summary>
+/// Zusammenfassungsbeschreibung für REST
+/// </summary>
+public partial class REST
+{
+	[OperationContract]
+	[WebInvoke(Method = "POST", UriTemplate = "ConvertExcelToTable")]
+	public string[][] ConvertExcelToTable(System.IO.Stream contentStream)
+    {
+        try
+        {
+            System.IO.MemoryStream stream = new System.IO.MemoryStream();
+            contentStream.CopyTo(stream);
+            stream.Position = 0;
+            System.IO.File.WriteAllBytes(@"C:\Users\Michael\AppData\Local\Temp\0.xlsx", stream.ToArray());
+            stream.Position = 0;
+
+            return CmdbAPI.BusinessLogic.Helpers.ExcelHelper.GetLinesFromExcelDocument(stream).ToArray();
+        }
+        catch (Exception ex)
+        {
+            ServerError();
+            return null;
+        }
+    }
+
+    [OperationContract]
+    [WebInvoke(Method = "POST", UriTemplate = "ConvertCsvToTable")]
+    public string[][] ConvertCsvToTable(System.IO.Stream contentStream)
+    {
+        try
+        {
+            return CmdbAPI.BusinessLogic.Helpers.ExcelHelper.GetLinesFromCSV(contentStream).ToArray();
+        }
+        catch (Exception)
+        {
+            ServerError();
+            return null;
+        }
+    }
+}
--- ng-cmdb/src/app/display/configuration-item/import-items/import-items.component.ts
+++ ng-cmdb/src/app/display/configuration-item/import-items/import-items.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Store, select } from '@ngrx/store';
 import { Actions, ofType } from '@ngrx/effects';
 import { Observable, of } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
 
 import * as fromApp from 'src/app/shared/store/app.reducer';
 import * as fromSelectMetaData from 'src/app/shared/store/meta-data.selectors';
 
 import { Guid } from 'src/app/shared/guid';
+import { getUrl, getHeader } from 'src/app/shared/store/functions';
 
 @Component({
   selector: 'app-import-items',
@@ -18,7 +20,7 @@ import { Guid } from 'src/app/shared/guid';
 })
 export class ImportItemsComponent implements OnInit {
   form: FormGroup;
-  fileToUpload: File = null;
+  @ViewChild('file', {static: false}) file: ElementRef;
 
   constructor(private router: Router,
               private actions$: Actions,
@@ -28,11 +30,11 @@ export class ImportItemsComponent implements OnInit {
 
   ngOnInit() {
     this.form = this.fb.group({
-      itemType: '',
+      itemType: ['', [Validators.required]],
       elements: new Array(['attributes']),
       ignoreExisting: false,
       headlines: true,
-      file: undefined,
+      file: ['', [Validators.required, this.validateFile.bind(this)]],
     });
   }
 
@@ -42,21 +44,43 @@ export class ImportItemsComponent implements OnInit {
 
   onSubmit() {
     console.log(this.form.value);
-    console.log(this.fileToUpload);
   }
 
   handleFileInput(files: FileList) {
-    this.fileToUpload = files.item(0);
+    if (this.form.get('file').valid) {
+      // (this.file.nativeElement as HTMLInputElement).disabled = true;
+      this.postFile(files[0]).subscribe(data => {
+        console.log(data);
+      });
+    }
   }
 
-  postFile(fileToUpload: File) : Observable<boolean> {
-    const endpoint = 'your-destination-url';
+  validateFile(c: FormControl) {
+    if (this.file && this.file.nativeElement) {
+      const file = this.file.nativeElement as HTMLInputElement;
+      if (file && file.files && file.files.length > 0 && (file.files[0].type === 'text/csv' ||
+        file.files[0].type === 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' ||
+        (file.files[0].type === 'application/vnd.ms-excel' && file.files[0].name.endsWith('.csv')))) {
+          return null;
+      }
+    }
+    return 'invalid file type';
+  }
+
+  postFile(file: File): Observable<string[][]> {
+    let svc = '';
+    if (file.type === 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet') {
+      svc = 'ConvertExcelToTable';
+    }
+    if (file.type === 'text/csv' || (file.type === 'application/vnd.ms-excel' && file.name.endsWith('.csv'))) {
+      svc = 'ConvertCsvToTable';
+    }
+    const endpoint = getUrl(svc);
     const formData: FormData = new FormData();
-    formData.append('fileKey', fileToUpload, fileToUpload.name);
-    return of(true);
-    // return this.http.post(endpoint, formData, { headers: yourHeadersConfig })
-    //   .map(() => { return true; })
-    //   .catch((e) => this.handleError(e));
-}
+    formData.append('contentStream', file, file.name);
+    return this.http.post<string[][]>(endpoint, formData).pipe(
+      catchError((e) => of(null)),
+    );
+  }
 
 }
