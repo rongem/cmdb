35bde346ade8b5c3a5d71edb98bc449aaf154f5d
Michael Ronge <github@ronge.org>
2019-11-26 17:08:47 +0100

    work in progress: export

---
+++ CMDB/App_Code/REST.Export.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Web;
+
+/// <summary>
+/// Zusammenfassungsbeschreibung für REST
+/// </summary>
+public partial class REST
+{
+    [OperationContract]
+    [WebGet(UriTemplate = "Export/{id}/{depth}/{format}")]
+    public Stream Export(string id, string depth, string format)
+    {
+        Guid guid;
+        if (!Guid.TryParse(id, out guid))
+        {
+            BadRequest();
+            return null;
+        }
+
+        return new MemoryStream();
+    }
+}
