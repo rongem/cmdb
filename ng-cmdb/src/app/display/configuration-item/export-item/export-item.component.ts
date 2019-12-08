3bafb1710d0ed7eea283e0acf143d9628a8926f6
Michael Ronge <github@ronge.org>
2019-12-08 09:41:18 +0100

    using xlsx module doesn't produce valid excel files, too.

--- ng-cmdb/src/app/display/configuration-item/export-item/export-item.component.ts
+++ ng-cmdb/src/app/display/configuration-item/export-item/export-item.component.ts
@@ -1,9 +1,15 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Store, select } from '@ngrx/store';
+import { tap } from 'rxjs/operators';
+
+import * as fromApp from 'src/app/shared/store/app.reducer';
+import * as fromSelectDisplay from 'src/app/display/store/display.selectors';
 
 import { Guid } from 'src/app/shared/guid';
 import { getUrl } from 'src/app/shared/store/functions';
+import { ExcelService } from 'src/app/display/shared/excel.service';
 
 @Component({
   selector: 'app-export-item',
@@ -18,7 +24,9 @@ export class ExportItemComponent implements OnInit {
   constructor(public dialogRef: MatDialogRef<ExportItemComponent>,
               @Inject(MAT_DIALOG_DATA) public data: Guid,
               public dialog: MatDialog,
-              private http: HttpClient) { }
+              private http: HttpClient,
+              private store: Store<fromApp.AppState>,
+              private excel: ExcelService) { }
 
   ngOnInit() {
   }
@@ -37,17 +45,50 @@ export class ExportItemComponent implements OnInit {
     }
   }
 
+  get configurationItem() {
+    return this.store.select(fromSelectDisplay.selectDisplayConfigurationItem);
+  }
+
   downloadFile(accept: string) {
-    const urlPart = this.exportData === 'connections' ? 'Connections' : 'Links';
-    this.http.get(getUrl('Export/Table/ForItem/' + urlPart + '/' + this.data),
-      { headers: new HttpHeaders({ 'Accept' : accept}) }
-    ).subscribe((data) => {
-      console.log(data);
-      const blob = new Blob([data.toString()], { type: accept });
-      console.log(blob);
-      const url = window.URL.createObjectURL(blob);
-      window.open(url);
+    const sub = this.configurationItem.subscribe(item => {
+      const param: {
+        upperItemType: string;
+        upperItemName: string;
+        connectionType: string;
+        lowerItemType: string;
+        lowerItemName: string;
+        description: string;
+      }[] = [];
+      item.connectionsToLower.forEach(conn => param.push({
+        upperItemType: item.type,
+        upperItemName: item.name,
+        connectionType: conn.connectionType,
+        lowerItemType: conn.targetType,
+        lowerItemName: conn.targetName,
+        description: conn.description,
+      }));
+      item.connectionsToUpper.forEach(conn => param.push({
+        upperItemType: conn.targetType,
+        upperItemName: conn.targetName,
+        connectionType: conn.connectionType,
+        lowerItemType: item.type,
+        lowerItemName: item.name,
+        description: conn.description,
+      }));
+      this.excel.exportAsExcelFile(param, 'download.xslx');
+      this.dialogRef.close();
+      sub.unsubscribe();
     });
+    // const urlPart = this.exportData === 'connections' ? 'Connections' : 'Links';
+    // this.http.get(getUrl('Export/Table/ForItem/' + urlPart + '/' + this.data),
+    //   { headers: new HttpHeaders({ 'Accept' : accept}) }
+    // ).subscribe((data) => {
+    //   console.log(data);
+    //   const blob = new Blob([data.toString()], { type: accept });
+    //   console.log(blob);
+    //   const url = window.URL.createObjectURL(blob);
+    //   window.open(url);
+    // });
   }
 
   downloadGraph() {
