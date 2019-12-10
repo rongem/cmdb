62e7c8364da0e0feb275da14d5c35d644d5306f6
Michael Ronge <github@ronge.org>
2019-12-10 14:52:46 +0100

    export of excel works now

--- ng-cmdb/src/app/display/shared/excel.service.ts
+++ ng-cmdb/src/app/display/shared/excel.service.ts
@@ -13,14 +13,6 @@ export class ExcelService {
     public exportAsExcelFile(json: any[], excelFileName: string): void {
         const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(json);
         const workbook: XLSX.WorkBook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
-        const excelBuffer: any = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
-        console.log(excelBuffer);
-        this.saveAsExcelFile(excelBuffer, excelFileName);
-    }
-
-    private saveAsExcelFile(buffer: any, fileName: string): void {
-        const data: Blob = new Blob([buffer], {type: EXCEL_TYPE});
-        console.log(data);
-        FileSaver.saveAs(data, fileName + '_export_' + new  Date().getTime() + EXCEL_EXTENSION);
+        XLSX.writeFile(workbook, 'export.xlsx');
     }
 }
