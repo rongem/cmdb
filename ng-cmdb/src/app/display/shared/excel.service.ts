bb826954ac6aedcb2267a73560d48ea7de79c7f3
Michael Ronge <github@ronge.org>
2019-12-15 19:28:04 +0100

    wip

--- ng-cmdb/src/app/display/shared/excel.service.ts
+++ ng-cmdb/src/app/display/shared/excel.service.ts
@@ -15,4 +15,22 @@ export class ExcelService {
         const workbook: XLSX.WorkBook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
         XLSX.writeFile(workbook, 'export.xlsx');
     }
+
+    public exportAsCsvFile(data: any) {
+        const replacer = (key, value) => value === null ? '' : value; // specify how you want to handle null values here
+        const header = Object.keys(data[0]);
+        let csv = data.map(row => header.map(fieldName => JSON.stringify(row[fieldName], replacer)).join(','));
+        csv.unshift(header.join(','));
+        let csvArray = csv.join('\r\n');
+    
+        var a = document.createElement('a');
+        var blob = new Blob([csvArray], {type: 'text/csv' }),
+        url = window.URL.createObjectURL(blob);
+    
+        a.href = url;
+        a.download = "myFile.csv";
+        a.click();
+        window.URL.revokeObjectURL(url);
+        a.remove();
+    }
 }
