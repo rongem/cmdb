8f3cf7126ce25ea86489f4cebc0c0e5eec24c118
Michael Ronge <github@ronge.org>
2020-03-12 08:10:04 +0100

    ng-cmdb: overhauled translation and created xlf file

--- ng-dcman/src/app/admin/model/model.component.ts
+++ ng-dcman/src/app/admin/model/model.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { Store, select } from '@ngrx/store';
+import { map, withLatestFrom } from 'rxjs/operators';
+
+import * as fromSelectMetaData from 'src/app/shared/store/meta-data.selectors';
+import * as fromSelectBasics from 'src/app/shared/store/basics/basics.selectors';
+import * as fromSelectAsset from 'src/app/shared/store/asset/asset.selectors';
+import * as MetaDataActions from 'src/app/shared/store/meta-data.actions';
+
+import { AppState } from 'src/app/shared/store/app.reducer';
+import { getRouterState } from 'src/app/shared/store/router/router.reducer';
 
 @Component({
   selector: 'app-model',
@@ -7,7 +17,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModelComponent implements OnInit {
 
-  constructor() { }
+  constructor(private store: Store<AppState>) { }
 
   ngOnInit(): void {
   }
