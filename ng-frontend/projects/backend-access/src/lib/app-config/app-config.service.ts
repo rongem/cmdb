804775dc84e020ec312a3cddc83ed4f4879761ee
Michael Ronge <github@ronge.org>
2020-04-13 09:11:21 +0200

    corrected one last error for production compiling

--- ng-frontend/projects/backend-access/src/lib/app-config/app-config.service.ts
+++ ng-frontend/projects/backend-access/src/lib/app-config/app-config.service.ts
@@ -4,7 +4,7 @@ import { AppConfig } from './app-config.model';
 
 @Injectable({providedIn: 'root'})
 export class AppConfigService {
-    static settings: AppConfig;
+    static settings: AppConfig = null;
     static validURL(url: string) {
         const pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
           '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
