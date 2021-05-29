3d5e4f993f15473135f3ba6251cc5656ce86ca1e
Michael Ronge <github@ronge.org>
2021-05-29 12:17:48 +0200

    added error handling for dcman

--- ng-frontend/projects/dcman/src/main.ts
+++ ng-frontend/projects/dcman/src/main.ts
@@ -9,4 +9,18 @@ if (environment.production) {
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+  .catch(err => {
+    const errorMsgElement = document.querySelector('#errorMsgElement');
+    let message = 'Could not start application: ';
+    if (err) {
+        if (err.message) {
+            message = message + ': ' + err.message;
+        } else {
+            if (err.startsWith('error: Http failure response for http') && err.endsWith(': 0 Unknown Error')) {
+              err = 'The backend is not available. Please inform your administrator. ' + err;
+            }
+            message = message + ': ' + err;
+        }
+    }
+    errorMsgElement.textContent = message;
+  });
