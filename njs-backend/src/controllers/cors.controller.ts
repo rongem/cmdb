6133fafdfa6c41fe05f8df37065738793eb3b026
Michael Ronge <github@ronge.org>
2021-05-31 09:55:32 +0200

    fixed a cores mismatch

--- njs-backend/src/controllers/cors.controller.ts
+++ njs-backend/src/controllers/cors.controller.ts
@@ -1,10 +1,12 @@
 import { Request, Response, NextFunction } from 'express';
 
 export function preventCORSError(req: Request, res: Response, next: NextFunction) {
-    res.setHeader('Access-Control-Allow-Origin', req.headers.origin ?? '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    if (req.headers.origin) {
+        res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        res.setHeader('Access-Control-Allow-Credentials', 'true');
+    }
     if (req.method === 'OPTIONS') {
         res.end();
     } else {
