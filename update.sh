48fd7c24896068e34f2aeafa787cf5fc5e497bb2
Michael Ronge <github@ronge.org>
2021-09-07 15:31:52 +0200

    added update.sh

---
+++ update.sh
@@ -0,0 +1,12 @@
+npm update -g
+cd ng-frontend
+ng update @angular/cli @angular/core @angular/material @ngrx/store
+npm update
+npm audit fix
+ng build backend-access
+ng build
+cd ../njs-backend/
+npm update
+tsc --declaration && npm run test
+npm outdate
+read -n 1
