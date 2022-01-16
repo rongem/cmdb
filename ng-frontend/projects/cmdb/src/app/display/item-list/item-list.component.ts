0dd281bf9edf5d2c2a2c197b7d3d37ad2f3a4fc2
Michael Ronge <github@ronge.org>
2022-01-16 16:28:02 +0100

    added attributes to item list

--- ng-frontend/projects/cmdb/src/app/display/item-list/item-list.component.ts
+++ ng-frontend/projects/cmdb/src/app/display/item-list/item-list.component.ts
@@ -1,17 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { AttributeType, ConnectionType } from 'backend-access';
-import { take, withLatestFrom } from 'rxjs';
-import { ItemActions, ItemSelectors, SearchFormSelectors } from '../../shared/store/store.api';
+import { AttributeType, ConfigurationItem, ConnectionRule, ConnectionType, ItemType, MetaDataSelectors } from 'backend-access';
+import { iif, of, Subscription, switchMap, tap, withLatestFrom } from 'rxjs';
+import { ItemSelectors, SearchFormSelectors } from '../../shared/store/store.api';
 
 @Component({
   selector: 'app-item-list',
   templateUrl: './item-list.component.html',
   styleUrls: ['./item-list.component.scss']
 })
-export class ItemListComponent implements OnInit {
+export class ItemListComponent implements OnInit, OnDestroy {
   displayedAttributeTypesList: AttributeType[] = [];
   displayedConnectionTypesList: ConnectionType[] = [];
+
+  private subscription: Subscription;
 
   constructor(private store: Store) { }
 
@@ -19,7 +21,36 @@ export class ItemListComponent implements OnInit {
     return this.store.select(ItemSelectors.resultList);
   }
 
+  get searchItemType() {
+    return this.store.select(SearchFormSelectors.searchItemType);
+  }
+
   ngOnInit(): void {
+    this.subscription = this.searchItemType.pipe(
+      tap(itemType => console.log(itemType, !!itemType)),
+      switchMap(itemType => {
+        if (itemType) {
+          return this.store.select(MetaDataSelectors.selectAttributeTypesForItemType(itemType.id)).pipe(
+            withLatestFrom(this.store.select(MetaDataSelectors.selectConnectionRulesForLowerItemType(itemType)),
+              this.store.select(MetaDataSelectors.selectConnectionRulesForUpperItemType(itemType))
+            ),
+          );
+        }
+        return of(undefined);
+    })).subscribe(result => {
+      if (result) {
+        const [attributeTypes, rulesToUpper, rulesToLower] = result;
+        this.displayedAttributeTypesList = attributeTypes;
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+      this.subscription?.unsubscribe();
+  }
+
+  getAttributeValue(item: ConfigurationItem, attributeTypeId: string) {
+    return item.attributes?.find(a => a.typeId === attributeTypeId)?.value ?? '';
   }
 
 }
