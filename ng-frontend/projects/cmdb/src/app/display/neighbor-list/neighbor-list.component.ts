06adb70ddb988c6c2518666f0870a735446cc6fc
Michael Ronge <github@ronge.org>
2022-01-28 18:41:04 +0100

    trying to work with local object, but stuck with an error I don't understand

--- ng-frontend/projects/cmdb/src/app/display/neighbor-list/neighbor-list.component.ts
+++ ng-frontend/projects/cmdb/src/app/display/neighbor-list/neighbor-list.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
-import { iif, map, of, skipUntil, skipWhile, Subscription, switchMap, take, withLatestFrom } from 'rxjs';
-import { MetaDataSelectors, SearchConnection } from 'backend-access';
+import { iif, map, of, skipWhile, Subscription, switchMap, take, withLatestFrom } from 'rxjs';
+import { MetaDataSelectors, NeighborSearch, SearchActions, SearchConnection, SearchContent } from 'backend-access';
 import { ItemSelectors, NeighborSearchSelectors } from '../../shared/store/store.api';
 
 @Component({
@@ -10,8 +10,9 @@ import { ItemSelectors, NeighborSearchSelectors } from '../../shared/store/store
   templateUrl: './neighbor-list.component.html',
   styleUrls: ['./neighbor-list.component.scss']
 })
-export class NeighborListComponent implements OnInit {
+export class NeighborListComponent implements OnInit, OnDestroy {
   form: FormGroup;
+  search = new NeighborSearch();
   newFilterType = '';
   newNameOrValue = '';
   newAttributeType = '';
@@ -110,7 +111,7 @@ export class NeighborListComponent implements OnInit {
   }
 
   get extraSearch() {
-    return this.form.get('extraSearch');
+    return this.search.extraSearch ?? new SearchContent();
   }
 
   ngOnInit(): void {
@@ -125,64 +126,121 @@ export class NeighborListComponent implements OnInit {
         itemTypeId: this.fb.control(itemTypes[0]?.id ?? '', Validators.required),
         maxLevels: this.fb.control(1, [Validators.required, Validators.min(1), Validators.max(5)]),
         searchDirection: this.fb.control(0, [Validators.required, Validators.min(-1), Validators.max(1)]),
-        extraSearch: this.fb.group({
-          nameOrValue: this.fb.control(''),
-          attributes: this.fb.array([]),
-          connectionsToUpper: this.fb.array([]),
-          connectionsToLower: this.fb.array([]),
-          changedBefore: this.fb.control(''),
-          changedAfter: this.fb.control(''),
-          responsibleToken: this.fb.control(''),
-        })
+      });
+      this.search = {
+        sourceItem: item.id,
+        itemTypeId: itemTypes[0]?.id ?? '',
+        maxLevels: 1,
+        searchDirection: 0,
+        extraSearch: {
+          nameOrValue: undefined,
+          itemTypeId: itemTypes[0]?.id,
+          attributes: [],
+          connectionsToUpper: [],
+          connectionsToLower: [],
+        }
+      };
+      this.subscription = this.form.valueChanges.subscribe(formValue => {
+        if (this.search.itemTypeId !== formValue.itemTypeId) {
+          this.search.itemTypeId = formValue.itemTypeId;
+          this.search.extraSearch.itemTypeId = formValue.itemTypeId;
+          this.search.extraSearch.attributes = [];
+          this.search.extraSearch.connectionsToLower = [];
+          this.search.extraSearch.connectionsToUpper = [];
+        }
+        this.search.sourceItem = formValue.sourceItem;
+        this.search.maxLevels = +formValue.maxLevels;
+        this.search.searchDirection = +formValue.searchDirection;
+        this.performSearch();
       });
     });
   }
-  /*
-    this.fb.group({
-      typeId: this.fb.control('', Validators.required),
-      value: this.fb.control(''),
-    })
-    this.fb.group({
-      connectionTypeId: this.fb.control('', Validators.required),
-      itemTypeId: this.fb.control(''),
-      count: this.fb.control('1', Validators.required),
-    })
-    this.fb.group({
-      connectionTypeId: this.fb.control('', Validators.required),
-      itemTypeId: this.fb.control(''),
-      count: this.fb.control('1', Validators.required),
-    })
-  */
-   onChangeText() {
-    this.extraSearch.get('nameOrValue').setValue(this.newNameOrValue);
-    this.newFilterType = '';
-    this.newNameOrValue = this.getRelativeDate(-1, 0);
-   }
-   onDeleteText() {
-    this.extraSearch.get('nameOrValue').reset();
-   }
-   onAddAttribute() {
-     this.allowedAttributeTypeList.pipe(take(1)).subscribe(attributeTypes => {
-       (this.extraSearch.get('attributes') as FormArray).push(
-         this.fb.group({
-          typeId: this.fb.control(this.newAttributeType),
-          value: this.fb.control(this.newAttributeValue),
-        })
-       );
-       this.resetForm();
-     });
-   }
-   onDeleteAttribute(attributeTypeId: string) {}
-   onAddConnectionToLower() {}
-   onDeleteConnectionToLower(index: number) {}
-   onAddConnectionToUpper() {}
-   onDeleteConnectionToUpper(index: number) {}
-   onAddChangedBefore() {}
-   onDeleteChangedBefore() {}
-   onAddChangedAfter() {}
-   onDeleteChangedAfter() {}
-   onAddResponsibility() {}
-  onDeleteResponsibility() {}
+
+  ngOnDestroy(): void {
+      this.subscription?.unsubscribe();
+  }
+
+  onChangeText() {
+    this.extraSearch.nameOrValue = this.newNameOrValue;
+    this.resetForm();
+  }
+  onDeleteText() {
+    this.extraSearch.nameOrValue = undefined;
+  }
+
+  onAddAttribute() {
+    this.extraSearch.attributes = this.extraSearch.attributes.filter(a => a.typeId !== this.newAttributeType);
+    this.extraSearch.attributes.push({
+      typeId: this.newAttributeType,
+      value: this.newAttributeValue,
+    });
+    this.resetForm();
+  }
+  onDeleteAttribute(attributeTypeId: string) {
+    this.extraSearch.attributes = this.extraSearch.attributes.filter(a => a.typeId !== attributeTypeId);
+  }
+
+  onAddConnectionToLower() {
+    this.extraSearch.connectionsToLower = this.extraSearch.connectionsToLower.filter(connection => !(this.newItemTypeToLower ?
+      connection.connectionTypeId === this.newConnectionTypeToLower && (!connection.itemTypeId || connection.itemTypeId === this.newItemTypeToLower) :
+      connection.connectionTypeId === this.newConnectionTypeToLower)
+    );
+    this.extraSearch.connectionsToLower.push({
+      connectionTypeId: this.newConnectionTypeToLower,
+      itemTypeId: this.newItemTypeToLower,
+      count: this.newConnectionCountToLower,
+    });
+    this.resetForm();
+  }
+  onDeleteConnectionToLower(index: number) {
+    this.extraSearch.connectionsToLower.splice(index, 1);
+  }
+  onAddConnectionToUpper() {
+    this.extraSearch.connectionsToUpper = this.extraSearch.connectionsToUpper.filter(connection => !(this.newItemTypeToUpper ?
+      connection.connectionTypeId === this.newConnectionTypeToUpper && (!connection.itemTypeId || connection.itemTypeId === this.newItemTypeToUpper) :
+      connection.connectionTypeId === this.newConnectionTypeToUpper)
+    );
+    this.extraSearch.connectionsToUpper.push({
+      connectionTypeId: this.newConnectionTypeToUpper,
+      itemTypeId: this.newItemTypeToUpper,
+      count: this.newConnectionCountToUpper,
+    });
+    this.resetForm();
+  }
+  onDeleteConnectionToUpper(index: number) {
+    this.extraSearch.connectionsToUpper.splice(index, 1);
+  }
+
+  onAddChangedBefore() {
+    this.extraSearch.changedBefore = new Date(Date.parse(this.newChangedBefore));
+    if (this.extraSearch.changedAfter && this.extraSearch.changedAfter.valueOf() < this.extraSearch.changedBefore.valueOf()) {
+      this.extraSearch.changedAfter = undefined;
+    }
+    this.resetForm();
+  }
+  onDeleteChangedBefore() {
+    this.extraSearch.changedBefore = undefined;
+  }
+
+  onAddChangedAfter() {
+    this.extraSearch.changedAfter = new Date(Date.parse(this.newChangedAfter));
+    if (this.extraSearch.changedBefore && this.extraSearch.changedAfter.valueOf() < this.extraSearch.changedBefore.valueOf()) {
+      this.extraSearch.changedBefore = undefined;
+    }
+    this.resetForm();
+  }
+  onDeleteChangedAfter() {
+    this.extraSearch.changedAfter = undefined;
+  }
+
+  onAddResponsibility() {
+    this.userName.pipe(take(1)).subscribe(userName => {
+      this.extraSearch.responsibleToken = userName;
+    });
+  }
+  onDeleteResponsibility() {
+    this.extraSearch.responsibleToken = undefined;
+  }
 
   getAttributeTypeName(typeId: string) {
     return this.store.select(MetaDataSelectors.selectSingleAttributeType(typeId)).pipe(map(at => at.name));
@@ -230,6 +288,11 @@ export class NeighborListComponent implements OnInit {
     this.newAttributeValue = '';
     this.newChangedBefore = this.getRelativeDate(-1, 0);
     this.newChangedAfter = this.getRelativeDate(0, -1);
+  }
+
+  private performSearch() {
+    console.log(this.search);
+    this.store.dispatch(SearchActions.performNeighborSearch({searchContent: this.search}));
   }
 
   private getRelativeDate(days: number, months: number) {
