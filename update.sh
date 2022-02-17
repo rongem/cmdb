f61db954c4e344cabb4d342127f4eab29f17a0b3
Michael Ronge <github@ronge.org>
2022-02-17 06:45:01 +0100

    ng update & npm update

--- update.sh
+++ update.sh
@@ -3,10 +3,12 @@ cd ng-frontend
 ng update @angular/cli @angular/core @angular/cdk @ngrx/store @angular-eslint/schematics
 npm update
 npm audit fix
+grunt bump
 ng build backend-access
 ng build
 cd ../njs-backend/
 npm update
+grunt bump
 tsc --declaration && npm run test
 npm outdate
 read -n 1
