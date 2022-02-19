fd97b40e3c76cd86b40bce1725ea4136fa5b2128
Michael Ronge <github@ronge.org>
2022-02-19 17:27:41 +0100

    working on translation

--- ng-frontend/projects/cmdb/src/app/shared/store/multi-edit/multi-edit.selectors.ts
+++ ng-frontend/projects/cmdb/src/app/shared/store/multi-edit/multi-edit.selectors.ts
@@ -48,22 +48,6 @@ export const selectResultListFullColumnsForSearchItemType = createSelector(Searc
     }
 );
 
-export const selectResultListFullColumnsForSearchItemType = createSelector(SearchFormSelectors.attributeTypesForCurrentSearchItemType,
-    SearchFormSelectors.connectionRulesForCurrentIsLowerSearchItemType, SearchFormSelectors.connectionRulesForCurrentIsUpperSearchItemType,
-    MetaDataSelectors.selectConnectionTypes, MetaDataSelectors.selectItemTypes,
-    (attributeTypes, connectionRulesToUpper, connectionRulesToLower, connectionTypes, itemTypes) => {
-        const array: KeyValue<string, string>[] = [];
-        attributeTypes.forEach(at => array.push({key: 'a:' + at.id, value: at.name}));
-        connectionRulesToLower.forEach(cr => array.push({key: 'ctl:' + cr.id, value:
-            connectionTypes.find(c => c.id === cr.connectionTypeId).name + ' ' +
-            itemTypes.find(i => i.id === cr.lowerItemTypeId).name}));
-        connectionRulesToUpper.forEach(cr => array.push({key: 'ctu:' + cr.id, value:
-            connectionTypes.find(c => c.id === cr.connectionTypeId).reverseName + ' ' +
-            itemTypes.find(i => i.id === cr.upperItemTypeId).name}));
-        return array;
-    }
-);
-
 export const selectOperationsLeft = createSelector(idsToProcess, ids => ids.length);
 
 export const idPresent = (id: string) => createSelector(idsToProcess, ids => ids.includes(id));
