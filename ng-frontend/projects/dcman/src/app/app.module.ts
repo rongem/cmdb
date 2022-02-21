c086d3a5a0672fa81e5ef30aaf8ee81f2a9930ab
Michael Ronge <github@ronge.org>
2022-02-21 06:52:15 +0100

    fixed an error in dcman

--- ng-frontend/projects/dcman/src/app/app.module.ts
+++ ng-frontend/projects/dcman/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
-import { AuthInterceptor, MetaDataEffects, ValidatorModule } from 'backend-access';
+import { AuthInterceptor, EnvServiceProvider, MetaDataEffects, ValidatorModule } from 'backend-access';
 
 import { registerLocaleData } from '@angular/common';
 import localeEn from '@angular/common/locales/en';
@@ -59,6 +59,7 @@ registerLocaleData(localeEn);
   ],
   providers: [
     Title,
+    EnvServiceProvider,
     {
       provide: APP_INITIALIZER,
       useFactory: initializeApp,
