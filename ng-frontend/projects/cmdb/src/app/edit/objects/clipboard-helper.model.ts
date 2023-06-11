0b39d3fee467fe5d7c0adbd8836c7c6c1be6be23
Michael Ronge <devel@ronge.org>
2023-06-11 08:05:39 +0200

    fixed vulnerability in clipboard-helper model

--- ng-frontend/projects/cmdb/src/app/edit/objects/clipboard-helper.model.ts
+++ ng-frontend/projects/cmdb/src/app/edit/objects/clipboard-helper.model.ts
@@ -2,17 +2,18 @@ export class ClipboardHelper {
     static getTableContent(data: DataTransfer): string[][] {
         // first, try html
         let result = data.getData('text/html');
-        if (result.length > 0 && !result.includes('<script>')) {
+        ClipboardHelper.sanitize(result);
+        if (result.length > 0) {
             const dom = new DOMParser().parseFromString(result, 'text/html');
             const table = dom.querySelector('table');
             // only, if a table element exists
             if (table) {
                 // get rows
-                const rows = table.querySelectorAll('tr');
-                if (rows.length > 0) {
-                    const lines: string[][] = [];
-                    // translate rows to lines
-                    rows.forEach((r, i) => {
+                const tableRows = table.querySelectorAll('tr');
+                if (tableRows.length > 0) {
+                    const rows: string[][] = [];
+                    // translate table rows to simple rows
+                    tableRows.forEach((r, i) => {
                         const cells = r.querySelectorAll('td');
                         const cols: string[] = [];
                         cells.forEach(c => {
@@ -25,14 +26,14 @@ export class ClipboardHelper {
                             const text = c.textContent ?? '';
                             cols.push(text);
                         });
-                        lines.push(cols);
+                        rows.push(cols);
                     });
-                    if (lines.length > 0) {
+                    if (rows.length > 0) {
                         // check, if all rows have the same number of columns
                         // if not, try fallback
-                        const lengthes = [...new Set(lines.map(l => l.length))];
+                        const lengthes = [...new Set(rows.map(l => l.length))];
                         if (lengthes.length === 1 && lengthes[0] > 0) {
-                            return lines;
+                            return rows;
                         }
                     }
                 }
@@ -41,16 +42,27 @@ export class ClipboardHelper {
         // fallback solution, if html delivers no valid data
         result = data.getData('text/plain');
         if (result.length > 0) {
-            const lines = result.split('\n').map(r => r.split('\t'));
-            if (lines.length > 0) {
+            const rows = result.split('\n').map(r => r.split('\t'));
+            if (rows.length > 0) {
                 // check, if all rows have the same number of columns
                 // if not, fail
-                const lengthes = [...new Set(lines.map(l => l.length))];
+                const lengthes = [...new Set(rows.map(l => l.length))];
                 if (lengthes.length === 1 && lengthes[0] > 0) {
-                    return lines;
+                    return rows;
                 }
             }
         }
         throw new Error('No valid table could be retrieved from clipboard.');
+    }
+
+    private static sanitize(result: string) {
+        const regexTag = /\<(applet|audio|embed|object|script|video)/gmi
+        const regexAttr = /\<[^>]+\s(on|action|background="http|cite|code|data|formaction|href="http|icon|longdesk|manifest|profile|src|usemap)/gmi;
+        if (regexTag.test(result)) {
+            throw new Error('Script content is forbidden due to prevention of XSS. Problematic tags found.');
+        }
+        if (regexAttr.test(result)) {
+            throw new Error('Script content is forbidden due to prevention of XSS. problematic attributes found.');
+        }
     }
 }
